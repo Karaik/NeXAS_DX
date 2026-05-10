f6bd9c90ff752d46b812ce0ff254c9bc71a54f6f
karaik <3025067003@qq.com>
2026-05-10 11:16:13 +0800

    clarias CEventEscape

--- src/main/java/com/giga/nexas/dto/clarias/waz/wazfactory/wazinfoclass/obj/CEventEscape.java
+++ src/main/java/com/giga/nexas/dto/clarias/waz/wazfactory/wazinfoclass/obj/CEventEscape.java
@@ -25,13 +25,13 @@ public class CEventEscape extends SkillInfoObject {
     public static final CEventEscapeType[] CEVENT_ESCAPE_ENTRIES = {
             new CEventEscapeType(0xFFFFFFFF, "タイプ"),
             new CEventEscapeType(0xFFFFFFFF, "優先順位"),
-            new CEventEscapeType(0x0B, "チェック方向"),
+            new CEventEscapeType(0x0E, "チェック方向"),
             new CEventEscapeType(0xFFFFFFFF, "チェック方向補正"),
             new CEventEscapeType(0xFFFFFFFF, "チェック範囲"),
             new CEventEscapeType(0xFFFFFFFF, "チェック範囲（高さ）"),
             new CEventEscapeType(0xFFFFFFFF, "チェック高度補正"),
             new CEventEscapeType(0xFFFFFFFF, "チェック回数"),
-            new CEventEscapeType(0x0B, "回避方向"),
+            new CEventEscapeType(0x0E, "回避方向"),
             new CEventEscapeType(0xFFFFFFFF, "回避方向補正"),
             new CEventEscapeType(0xFFFFFFFF, "短打撃"),
             new CEventEscapeType(0xFFFFFFFF, "突進"),
@@ -50,46 +50,81 @@ public class CEventEscape extends SkillInfoObject {
         private SkillInfoObject data;
     }
 
+    private Integer int1;
+    private Integer int2;
+    private Integer int3;
+    private Integer int4;
+    private Integer int5;
+    private Integer int6;
+    private Integer int7;
+    private Integer int8;
     private List<CEventEscapeUnit> unitList = new ArrayList<>();
 
-    public CEventEscape(Integer typeId) { super(typeId); }
+    public CEventEscape(Integer typeId) {
+        super(typeId);
+    }
 
     @Override
     public void readInfo(BinaryReader reader) {
         super.readInfo(reader);
+
+        this.int1 = reader.readInt();
+        this.int2 = reader.readInt();
+        this.int3 = reader.readInt();
+        this.int4 = reader.readInt();
+        this.int5 = reader.readInt();
+        this.int6 = reader.readInt();
+        this.int7 = reader.readInt();
+        this.int8 = reader.readInt();
+
         this.unitList.clear();
-        for (int i = 0; i < 14; i++) {
-            int buffer = reader.readInt();
-            CEventEscapeUnit unit = new CEventEscapeUnit();
-            unit.setUnitSlotNum(i);
-            unit.setBuffer(buffer);
-            unit.setDescription(CEVENT_ESCAPE_ENTRIES[i].getDescription());
-            int innerTypeId = CEVENT_ESCAPE_ENTRIES[i].getType();
-            if (buffer != 0) {
-                SkillInfoObject obj = createCEventObjectByTypeClarias(innerTypeId);
-                obj.readInfo(reader);
-                unit.setData(obj);
+        for (int i = 0; i < 10; i++) {
+            if (i == 2 || i == 8) {
+                int buffer = reader.readInt();
+                CEventEscapeUnit unit = new CEventEscapeUnit();
+                unit.setUnitSlotNum(i);
+                unit.setBuffer(buffer);
+                unit.setDescription(CEVENT_ESCAPE_ENTRIES[i].getDescription());
+                if (buffer != 0) {
+                    SkillInfoObject obj = createCEventObjectByTypeClarias(CEVENT_ESCAPE_ENTRIES[i].getType());
+                    obj.readInfo(reader);
+                    unit.setData(obj);
+                }
+                this.unitList.add(unit);
             }
-            this.unitList.add(unit);
         }
     }
 
     @Override
     public void writeInfo(BinaryWriter writer) throws IOException {
         super.writeInfo(writer);
-        for (int i = 0; i < 14; i++) {
-            CEventEscapeUnit target = null;
-            for (CEventEscapeUnit unit : this.unitList) {
-                if (unit.getUnitSlotNum() == i) { target = unit; break; }
+        writer.writeInt(this.int1);
+        writer.writeInt(this.int2);
+        writer.writeInt(this.int3);
+        writer.writeInt(this.int4);
+        writer.writeInt(this.int5);
+        writer.writeInt(this.int6);
+        writer.writeInt(this.int7);
+        writer.writeInt(this.int8);
+
+        CEventEscapeUnit slot2 = null;
+        CEventEscapeUnit slot8 = null;
+        for (CEventEscapeUnit unit : this.unitList) {
+            if (unit.getUnitSlotNum() == 2) {
+                slot2 = unit;
+            } else if (unit.getUnitSlotNum() == 8) {
+                slot8 = unit;
             }
-            if (target != null) {
-                writer.writeInt(target.getBuffer());
-                if (target.getBuffer() != 0 && target.getData() != null) {
-                    target.getData().writeInfo(writer);
-                }
-            } else {
-                writer.writeInt(0);
-            }
+        }
+
+        writer.writeInt(slot2.getBuffer());
+        if (slot2.getBuffer() != 0) {
+            slot2.getData().writeInfo(writer);
+        }
+
+        writer.writeInt(slot8.getBuffer());
+        if (slot8.getBuffer() != 0) {
+            slot8.getData().writeInfo(writer);
         }
     }
 }
